e6d731b073a1269e43a0cc4595cb22f5bd3245f1
Dan <dan@Dans-MacBook-Pro.local>
2025-07-29 14:43:30 +0700

    refactor code

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import {
@@ -15,9 +16,9 @@ import {
 } from 'src/common/constants/config';
 import { UserSessionService } from 'src/user-session/user-session.service';
 import { UserService } from 'src/user/user.service';
+import { RefreshDtoToken } from './dto/refresh-token.dto';
 import { SignInDto } from './dto/signin.dto';
 import { SignUpDto } from './dto/signUp.dto';
-import { RefreshDtoToken } from './dto/refresh-token.dto';
 
 @Injectable()
 export class AuthService {
@@ -26,30 +27,35 @@ export class AuthService {
     private readonly jwtService: JwtService,
     private readonly userSessionService: UserSessionService,
   ) {}
+
   async signUp(dto: SignUpDto) {
+    // kiểm tra xem email đã tồn tại chưa
     const existingUser = await this.userService.findByEmail(dto.email);
     if (existingUser) {
       throw new ConflictException('Email đã được sử dụng');
     }
+    // nếu chưa tồn tại thì băm mật khẩu và tạo người dùng mới
     const hashPassword = await bcrypt.hash(dto.password, 10);
-
     const user = await this.userService.create({
       ...dto,
       password: hashPassword,
     });
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { password, ...result } = user;
     return result;
   }
+
   async signIn(dto: SignInDto, req: Request) {
+    // kiểm tra xem email có tồn tại không
     const user = await this.userService.findByEmail(dto.email);
     if (!user) {
       throw new UnauthorizedException('Email không tồn tại');
     }
+    // nếu tồn tại thì so sánh mật khẩu
     const isMatch = await bcrypt.compare(dto.password, user.password);
     if (!isMatch) {
       throw new UnauthorizedException('Mật khẩu không chính xác');
     }
+    // nếu mật khẩu chính xác thì tạo access token và refresh token
     const payload = { id: user.id, email: user.email, role: user.role };
     const [accessToken, refreshToken] = await Promise.all([
       this.jwtService.signAsync(payload, {
@@ -59,8 +65,9 @@ export class AuthService {
       this.jwtService.signAsync(payload, {
         expiresIn: '7d',
         secret: refreshTokenSecret,
-      }), // Thời gian hết hạn của refresh token
+      }),
     ]);
+    // luư refresh token đã được mã hoá vào cơ sở dữ liệu
     const hashedToken = await bcrypt.hash(refreshToken, 10);
     await this.userSessionService.create({
       userId: user.id,
@@ -71,20 +78,21 @@ export class AuthService {
     });
     return { accessToken, refreshToken };
   }
+
   async refreshToken(dto: RefreshDtoToken, req: Request) {
+    // kiểm tra xem có deviceId nào đang login phù hợp không?
     const session = await this.userSessionService.findByUserAndDevice(
       dto.deviceId,
     );
-
     if (!session) {
       throw new ForbiddenException('session không tồn tại');
     }
 
+    // nếu có thì so sánh refresh token có đúng không (vì lúc lưu  đã hash nên giờ phải compare)
     const isMatch = await bcrypt.compare(
       dto.refreshToken,
       session.refreshToken,
     );
-
     if (!isMatch) {
       await this.userSessionService.deleteByDevice(
         session.userId,
@@ -92,15 +100,15 @@ export class AuthService {
       );
       throw new ForbiddenException('RefreshToken không hợp lệ');
     }
-
+    // nếu đúng thì giải mã refresh token để lấy thông tin người dùng
     const payload = await this.jwtService.verifyAsync(dto.refreshToken, {
       secret: refreshTokenSecret,
     });
     const user = await this.userService.findOne(payload.id);
-
     if (!user) {
       throw new ForbiddenException('Người dùng không tồn tại');
     }
+    // nếu người dùng tồn tại thì tạo access token mới
     const accessToken = await this.jwtService.signAsync(
       { id: payload.id, email: payload.email, role: payload.role },
       {
